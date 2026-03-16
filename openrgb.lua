eeef67e76e11d9ea62599b84e445f8596bf1e61a
Aditya Gupta <adi1998.2010@gmail.com>
2026-03-16 21:43:43 +0530

    move helpers to utils.lua, add more err checks

--- openrgb.lua
+++ openrgb.lua
@@ -1,5 +1,5 @@
 local socket = require("socket")
-local bit32 = require("bit32")
+local utils = require("utils")
 
 local OpenRGB = {}
 OpenRGB.__index = OpenRGB
@@ -9,207 +9,19 @@ local PORT = 6742
 
 -- Packet IDs enum
 local PKT = {
-    REQUEST_CONTROLLER_COUNT = 0,
-    REQUEST_CONTROLLER_DATA = 1,
-    REQUEST_PROTOCOL_VERSION = 40,
-    SET_CLIENT_NAME = 50,
+    NET_PACKET_ID_REQUEST_CONTROLLER_COUNT = 0,
+    NET_PACKET_ID_REQUEST_CONTROLLER_DATA = 1,
+    NET_PACKET_ID_REQUEST_PROTOCOL_VERSION = 40,
+    NET_PACKET_ID_SET_CLIENT_NAME = 50,
     NET_PACKET_ID_RGBCONTROLLER_UPDATELEDS = 1050,
     NET_PACKET_ID_RGBCONTROLLER_UPDATESINGLELED = 1052,
 }
 
-local function pack_u32(n)
-    local b1 = bit32.band(n , 0xff)
-    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
-    local b3 = bit32.band(bit32.rshift(n , 16) , 0xff)
-    local b4 = bit32.band(bit32.rshift(n , 24) , 0xff)
-    return string.char(b1, b2, b3, b4)
-end
-
-local function pack_i32(n)
-    local b1 = bit32.band(n , 0xff)
-    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
-    local b3 = bit32.band(bit32.rshift(n , 16) , 0xff)
-    local b4 = bit32.band(bit32.rshift(n , 24) , 0xff)
-    return string.char(b1, b2, b3, b4)
-end
-
-local function pack_u16(n)
-    local b1 = bit32.band(n , 0xff)
-    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
-    return string.char(b1, b2)
-end
-
-local function unpack_u32(s, pos)
-    pos = pos or 1
-    local b1, b2, b3, b4 = s:byte(pos, pos + 3)
-    local value =
-        b1 +
-        bit32.lshift(b2, 8) +
-        bit32.lshift(b3, 16) +
-        bit32.lshift(b4, 24)
-    return value, pos + 4
-end
-
-local function unpack_u32_arr(s, pos, n)
-    local arr = {}
-    for i = 1, n do
-        local u32; u32, pos = unpack_u32(s, pos)
-        table.insert(arr, u32)
-    end
-    return arr
-end
-
-local function unpack_u16(s, pos)
-    pos = pos or 1
-    local b1, b2 = s:byte(pos, pos + 1)
-    local value =
-        b1 +
-        bit32.lshift(b2, 8)
-    return value, pos + 2
-end
-
-local function unpack_i32(s, pos)
-    pos = pos or 1
-    local b1, b2, b3, b4 = s:byte(pos, pos + 3)
-    local value =
-        b1 +
-        bit32.lshift(b2, 8) +
-        bit32.lshift(b3, 16) +
-        bit32.lshift(b4, 24)
-    if value >= 2^31 then
-        value = value - 2^32
-    end
-
-    return value, pos + 4
-end
-
-local function unpack_string(s, pos, len)
-    local value = string.sub(s, pos, pos+len-2)
-    return value, pos+len
-end
-
-local function unpack_RGBColor(s, pos)
-    local rgb; rgb, pos = unpack_u32(s, pos)
-    local r = bit32.band(rgb, 0xff)
-    local g = bit32.band(bit32.rshift(rgb , 8) , 0xff)
-    local b = bit32.band(bit32.rshift(rgb , 16) , 0xff)
-    return { r = r, g = g, b = b}, pos
-end
-
-local function unpack_RGBColorN(s, pos, num_colors)
-    local colors = {}
-    for i = 1, num_colors do
-        local color; color, pos = unpack_RGBColor(s, pos)
-        table.insert(colors, color)
-    end
-    return colors, pos
-end
-
-local function pack_RGBColor(color)
-    return string.char(color.r, color.g, color.b, 0)
-end
-
-local function pack_RGBColorN(color_list)
-    local n = #color_list
-    local value = ""
-
-    for idx, color in ipairs(color_list) do
-        value = value .. pack_RGBColor(color)
-    end
-    return value
-end
-
-local function unpack_Mode_Data(s, pos, num_modes)
-    local modes = {}
-    for i = 1, num_modes do
-        local mode = {}
-        mode.mode_name_len, pos = unpack_u16(s, pos)
-        mode.mode_name, pos = unpack_string(s, pos, mode.mode_name_len)
-        mode.mode_value, pos = unpack_i32(s, pos)
-        mode.mode_flags, pos = unpack_u32(s, pos)
-        mode.mode_speed_min, pos = unpack_u32(s, pos)
-        mode.mode_speed_max, pos = unpack_u32(s, pos)
-        mode.mode_brightness_min, pos = unpack_u32(s, pos)
-        mode.mode_brightness_max, pos = unpack_u32(s, pos)
-        mode.mode_colors_min, pos = unpack_u32(s, pos)
-        mode.mode_colors_max, pos = unpack_u32(s, pos)
-        mode.mode_speed, pos = unpack_u32(s, pos)
-        mode.mode_brightness, pos = unpack_u32(s, pos)
-        mode.mode_direction, pos = unpack_u32(s, pos)
-        mode.mode_color_mode, pos = unpack_u32(s, pos)
-        mode.mode_num_colors, pos = unpack_u16(s, pos)
-        mode.mode_colors, pos = unpack_RGBColorN(s, pos, mode.mode_num_colors)
-        table.insert(modes,mode)
-    end
-    return modes, pos
-end
-
-local function unpack_Segment_Data(s, pos,  num_segments)
-    local segments = {}
-    for i = 1, num_segments do
-        local segment = {}
-        segment.segment_name_len, pos = unpack_u16(s, pos)
-        segment.segment_name, pos = unpack_string(s, pos, segment.segment_name_len)
-        segment.segment_type, pos = unpack_i32(s, pos)
-        segment.segment_start_idx, pos = unpack_u32(s, pos)
-        segment.segment_leds_count, pos = unpack_u32(s, pos)
-        table.insert(segments, segment)
-    end
-    return segments, pos
-end
-
-local function unpack_Zone_Data(s, pos, num_zones)
-    local zones = {}
-    for i = 1, num_zones do
-        local zone = {}
-        zone.zone_name_len, pos = unpack_u16(s, pos)
-        zone.zone_name, pos = unpack_string(s, pos, zone.zone_name_len)
-        zone.zone_type, pos = unpack_i32(s, pos)
-        zone.zone_leds_min, pos = unpack_u32(s, pos)
-        zone.zone_leds_max, pos = unpack_u32(s, pos)
-        zone.zone_leds_count, pos = unpack_u32(s, pos)
-        zone.zone_matrix_len, pos = unpack_u16(s, pos)
-        if zone.zone_matrix_len > 0 then
-            zone.zone_matrix_height, pos = unpack_u32(s, pos)
-            zone.zone_matrix_width, pos = unpack_u32(s, pos)
-            zone.zone_matrix_data, pos = unpack_u32_arr(s, pos, zone.zone_matrix_len - 8)
-        end
-        zone.num_segments, pos = unpack_u16(s, pos)
-        zone.segments, pos = unpack_Segment_Data(s, pos, zone.num_segments)
-        zone.zone_flags, pos = unpack_u32(s, pos)
-        table.insert(zones,zone)
-    end
-    return zones, pos
-end
-
-local function unpack_LED_Data(s, pos, num_leds)
-    local leds = {}
-    for i = 1, num_leds do
-        local led = {}
-        led.led_name_len, pos = unpack_u16(s, pos)
-        led.led_name, pos = unpack_string(s, pos, led.led_name_len)
-        led.led_value, pos = unpack_u32(s, pos)
-        table.insert(leds,led)
-    end
-    return leds, pos
-end
-
-local function unpack_LED_Alt_Names(s, pos, num_led_alt_names)
-    local alt_names = {}
-    for i = 1, num_led_alt_names do
-        local alt_name = {}
-        alt_name.led_alt_name_len, pos = unpack_u16(s, pos)
-        alt_name.led_alt_name, pos = unpack_string(s, pos, alt_name.led_alt_name_len)
-        table.insert(alt_names, alt_name)
-    end
-    return alt_names, pos
-end
-
 local function build_header(pkt_dev_idx, pkt_id, pkt_size)
     return MAGIC ..
-        pack_u32(pkt_dev_idx) ..
-        pack_u32(pkt_id) ..
-        pack_u32(pkt_size)
+        utils.pack_u32(pkt_dev_idx) ..
+        utils.pack_u32(pkt_id) ..
+        utils.pack_u32(pkt_size)
 end
 
 function OpenRGB.connect(host, port)
@@ -245,29 +57,29 @@ end
 
 function OpenRGB:recv_header()
     local data, err = self.sock:receive(16)
-    if not data then return nil end
+    if not data then return nil, err end
 
     local magic = data:sub(1,4)
 
     if magic ~= MAGIC then
-        error("Invalid OpenRGB packet")
+        return nil, "Invalid OpenRGB packet"
     end
 
     local pos = 5
-    local dev; dev, pos = unpack_u32(data, pos)
-    local pkt; pkt, pos = unpack_u32(data, pos)
-    local size; size, pos = unpack_u32(data, pos)
+    local dev; dev, pos = utils.unpack_u32(data, pos)
+    local pkt; pkt, pos = utils.unpack_u32(data, pos)
+    local size; size, pos = utils.unpack_u32(data, pos)
 
     return {
         dev = dev,
         pkt = pkt,
         size = size
-    }
+    }, nil
 end
 
 function OpenRGB:recv_payload(size)
     if size == 0 then
-        return ""
+        return "", nil
     end
 
     return self.sock:receive(size)
@@ -276,9 +88,9 @@ end
 function OpenRGB:negotiate_protocol(max_version)
     max_version = max_version or 5
 
-    local payload = pack_u32(max_version)
+    local payload = utils.pack_u32(max_version)
 
-    self:send_packet(0, PKT.REQUEST_PROTOCOL_VERSION, payload)
+    self:send_packet(0, PKT.NET_PACKET_ID_REQUEST_PROTOCOL_VERSION, payload)
 
     self.sock:settimeout(1)
 
@@ -289,9 +101,9 @@ function OpenRGB:negotiate_protocol(max_version)
         return 0
     end
 
-    local payload = self:recv_payload(header.size)
+    payload = self:recv_payload(header.size)
 
-    local server_ver = unpack_u32(payload)
+    local server_ver = utils.unpack_u32(payload)
 
     self.protocol_version = math.min(server_ver, max_version)
 
@@ -300,16 +112,16 @@ end
 
 function OpenRGB:set_client_name(name)
     local payload = name .. "\0"
-    self:send_packet(0, PKT.SET_CLIENT_NAME, payload)
+    self:send_packet(0, PKT.NET_PACKET_ID_SET_CLIENT_NAME, payload)
 end
 
 function OpenRGB:get_controller_count()
-    self:send_packet(0, PKT.REQUEST_CONTROLLER_COUNT, "")
+    self:send_packet(0, PKT.NET_PACKET_ID_REQUEST_CONTROLLER_COUNT, "")
 
     local header = self:recv_header()
     local payload = self:recv_payload(header.size)
 
-    local count = unpack_u32(payload)
+    local count = utils.unpack_u32(payload)
 
     return count
 end
@@ -330,71 +142,44 @@ function dump(o, depth)
     end
 end
 
-local function ParseControllerData(data)
-    local controller_data = {}
-    local pos = 1
-    controller_data.data_size, pos = unpack_u32(data, pos)
-    controller_data.type, pos = unpack_i32(data, pos)
-    controller_data.name_len, pos = unpack_u16(data, pos)
-    controller_data.name, pos = unpack_string(data, pos, controller_data.name_len)
-    controller_data.vendor_len, pos = unpack_u16(data, pos)
-    controller_data.vendor, pos = unpack_string(data, pos, controller_data.vendor_len)
-    controller_data.description_len, pos = unpack_u16(data, pos)
-    controller_data.description, pos = unpack_string(data, pos, controller_data.description_len)
-    controller_data.version_len, pos = unpack_u16(data, pos)
-    controller_data.version, pos = unpack_string(data, pos, controller_data.version_len)
-    controller_data.serial_len, pos = unpack_u16(data, pos)
-    controller_data.serial, pos = unpack_string(data, pos, controller_data.serial_len)
-    controller_data.location_len, pos = unpack_u16(data, pos)
-    controller_data.location, pos = unpack_string(data, pos, controller_data.location_len)
-    controller_data.num_modes, pos = unpack_u16(data, pos)
-    controller_data.active_mode, pos = unpack_i32(data, pos)
-    controller_data.modes, pos = unpack_Mode_Data(data, pos, controller_data.num_modes)
-    controller_data.num_zones, pos = unpack_u16(data, pos)
-    controller_data.zones, pos = unpack_Zone_Data(data, pos, controller_data.num_zones)
-    controller_data.num_leds, pos = unpack_u16(data, pos)
-    controller_data.leds, pos = unpack_LED_Data(data, pos, controller_data.num_leds)
-    controller_data.num_colors, pos = unpack_u16(data, pos)
-    controller_data.colors, pos = unpack_RGBColorN(data, pos, controller_data.num_colors)
-    controller_data.num_led_alt_names, pos = unpack_u16(data, pos)
-    controller_data.led_alt_names, pos = unpack_LED_Alt_Names(data, pos, controller_data.num_led_alt_names)
-    controller_data.flags, pos = unpack_u32(data, pos)
-    print(dump(controller_data))
-    return controller_data
-end
-
 function OpenRGB:get_controller_data(idx)
 
     local payload = ""
 
     if self.protocol_version > 0 then
-        payload = pack_u32(self.protocol_version)
+        payload = utils.pack_u32(self.protocol_version)
     end
 
-    self:send_packet(idx, PKT.REQUEST_CONTROLLER_DATA, payload)
+    self:send_packet(idx, PKT.NET_PACKET_ID_REQUEST_CONTROLLER_DATA, payload)
 
-    local header = self:recv_header()
-    payload = self:recv_payload(header.size)
+    local header, err = self:recv_header()
+    if not header then
+        return nil, err
+    end
+    payload, err = self:recv_payload(header.size)
+    if err then
+        return nil, err
+    end
 
-    local controller_data = ParseControllerData(payload)
+    local controller_data = utils.unpack_Controller_Data(payload)
 
-    return controller_data
+    return controller_data, nil
 end
 
 function OpenRGB:UpdateLEDs(pkt_dev_idx, led_color)
     local payload = ""
     local n = #led_color
-    payload = payload .. pack_u32(4 + 2 + 4*n) -- data_size
-    payload = payload .. pack_u16(n) -- num_colors
-    payload = payload .. pack_RGBColorN(led_color) -- led_color
+    payload = payload .. utils.pack_u32(4 + 2 + 4*n) -- data_size
+    payload = payload .. utils.pack_u16(n) -- num_colors
+    payload = payload .. utils.pack_RGBColorN(led_color) -- led_color
 
     self:send_packet(pkt_dev_idx, PKT.NET_PACKET_ID_RGBCONTROLLER_UPDATELEDS, payload)
 end
 
 function OpenRGB:UpdateSingleLEDs(pkt_dev_idx, led_idx, led_color)
     local payload = ""
-    payload = payload .. pack_i32(led_idx)
-    payload = payload .. pack_RGBColor(led_color)
+    payload = payload .. utils.pack_i32(led_idx)
+    payload = payload .. utils.pack_RGBColor(led_color)
 
     self:send_packet(pkt_dev_idx, PKT.NET_PACKET_ID_RGBCONTROLLER_UPDATESINGLELED, payload)
 end
---
+++ utils.lua
@@ -0,0 +1,224 @@
+local bit32 = require("bit32")
+
+local utils = {}
+
+function utils.pack_u32(n)
+    local b1 = bit32.band(n , 0xff)
+    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
+    local b3 = bit32.band(bit32.rshift(n , 16) , 0xff)
+    local b4 = bit32.band(bit32.rshift(n , 24) , 0xff)
+    return string.char(b1, b2, b3, b4)
+end
+
+function utils.pack_i32(n)
+    local b1 = bit32.band(n , 0xff)
+    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
+    local b3 = bit32.band(bit32.rshift(n , 16) , 0xff)
+    local b4 = bit32.band(bit32.rshift(n , 24) , 0xff)
+    return string.char(b1, b2, b3, b4)
+end
+
+function utils.pack_u16(n)
+    local b1 = bit32.band(n , 0xff)
+    local b2 = bit32.band(bit32.rshift(n , 8) , 0xff)
+    return string.char(b1, b2)
+end
+
+function utils.unpack_u32(s, pos)
+    pos = pos or 1
+    local b1, b2, b3, b4 = s:byte(pos, pos + 3)
+    local value =
+        b1 +
+        bit32.lshift(b2, 8) +
+        bit32.lshift(b3, 16) +
+        bit32.lshift(b4, 24)
+    return value, pos + 4
+end
+
+function utils.unpack_u32_arr(s, pos, n)
+    local arr = {}
+    for i = 1, n do
+        local u32; u32, pos = utils.unpack_u32(s, pos)
+        table.insert(arr, u32)
+    end
+    return arr
+end
+
+function utils.unpack_u16(s, pos)
+    pos = pos or 1
+    local b1, b2 = s:byte(pos, pos + 1)
+    local value =
+        b1 +
+        bit32.lshift(b2, 8)
+    return value, pos + 2
+end
+
+function utils.unpack_i32(s, pos)
+    pos = pos or 1
+    local b1, b2, b3, b4 = s:byte(pos, pos + 3)
+    local value =
+        b1 +
+        bit32.lshift(b2, 8) +
+        bit32.lshift(b3, 16) +
+        bit32.lshift(b4, 24)
+    if value >= 2^31 then
+        value = value - 2^32
+    end
+
+    return value, pos + 4
+end
+
+function utils.unpack_string(s, pos, len)
+    local value = string.sub(s, pos, pos+len-2)
+    return value, pos+len
+end
+
+function utils.unpack_RGBColor(s, pos)
+    local rgb; rgb, pos = utils.unpack_u32(s, pos)
+    local r = bit32.band(rgb, 0xff)
+    local g = bit32.band(bit32.rshift(rgb , 8) , 0xff)
+    local b = bit32.band(bit32.rshift(rgb , 16) , 0xff)
+    return { r = r, g = g, b = b}, pos
+end
+
+function utils.unpack_RGBColorN(s, pos, num_colors)
+    local colors = {}
+    for i = 1, num_colors do
+        local color; color, pos = utils.unpack_RGBColor(s, pos)
+        table.insert(colors, color)
+    end
+    return colors, pos
+end
+
+function utils.pack_RGBColor(color)
+    return string.char(color.r, color.g, color.b, 0)
+end
+
+function utils.pack_RGBColorN(color_list)
+    local n = #color_list
+    local value = ""
+
+    for idx, color in ipairs(color_list) do
+        value = value .. pack_RGBColor(color)
+    end
+    return value
+end
+
+function utils.unpack_Mode_Data(s, pos, num_modes)
+    local modes = {}
+    for i = 1, num_modes do
+        local mode = {}
+        mode.mode_name_len, pos = utils.unpack_u16(s, pos)
+        mode.mode_name, pos = utils.unpack_string(s, pos, mode.mode_name_len)
+        mode.mode_value, pos = utils.unpack_i32(s, pos)
+        mode.mode_flags, pos = utils.unpack_u32(s, pos)
+        mode.mode_speed_min, pos = utils.unpack_u32(s, pos)
+        mode.mode_speed_max, pos = utils.unpack_u32(s, pos)
+        mode.mode_brightness_min, pos = utils.unpack_u32(s, pos)
+        mode.mode_brightness_max, pos = utils.unpack_u32(s, pos)
+        mode.mode_colors_min, pos = utils.unpack_u32(s, pos)
+        mode.mode_colors_max, pos = utils.unpack_u32(s, pos)
+        mode.mode_speed, pos = utils.unpack_u32(s, pos)
+        mode.mode_brightness, pos = utils.unpack_u32(s, pos)
+        mode.mode_direction, pos = utils.unpack_u32(s, pos)
+        mode.mode_color_mode, pos = utils.unpack_u32(s, pos)
+        mode.mode_num_colors, pos = utils.unpack_u16(s, pos)
+        mode.mode_colors, pos = utils.unpack_RGBColorN(s, pos, mode.mode_num_colors)
+        table.insert(modes,mode)
+    end
+    return modes, pos
+end
+
+function utils.unpack_Segment_Data(s, pos,  num_segments)
+    local segments = {}
+    for i = 1, num_segments do
+        local segment = {}
+        segment.segment_name_len, pos = utils.unpack_u16(s, pos)
+        segment.segment_name, pos = utils.unpack_string(s, pos, segment.segment_name_len)
+        segment.segment_type, pos = utils.unpack_i32(s, pos)
+        segment.segment_start_idx, pos = utils.unpack_u32(s, pos)
+        segment.segment_leds_count, pos = utils.unpack_u32(s, pos)
+        table.insert(segments, segment)
+    end
+    return segments, pos
+end
+
+function utils.unpack_Zone_Data(s, pos, num_zones)
+    local zones = {}
+    for i = 1, num_zones do
+        local zone = {}
+        zone.zone_name_len, pos = utils.unpack_u16(s, pos)
+        zone.zone_name, pos = utils.unpack_string(s, pos, zone.zone_name_len)
+        zone.zone_type, pos = utils.unpack_i32(s, pos)
+        zone.zone_leds_min, pos = utils.unpack_u32(s, pos)
+        zone.zone_leds_max, pos = utils.unpack_u32(s, pos)
+        zone.zone_leds_count, pos = utils.unpack_u32(s, pos)
+        zone.zone_matrix_len, pos = utils.unpack_u16(s, pos)
+        if zone.zone_matrix_len > 0 then
+            zone.zone_matrix_height, pos = utils.unpack_u32(s, pos)
+            zone.zone_matrix_width, pos = utils.unpack_u32(s, pos)
+            zone.zone_matrix_data, pos = utils.unpack_u32_arr(s, pos, zone.zone_matrix_len - 8)
+        end
+        zone.num_segments, pos = utils.unpack_u16(s, pos)
+        zone.segments, pos = utils.unpack_Segment_Data(s, pos, zone.num_segments)
+        zone.zone_flags, pos = utils.unpack_u32(s, pos)
+        table.insert(zones,zone)
+    end
+    return zones, pos
+end
+
+function utils.unpack_LED_Data(s, pos, num_leds)
+    local leds = {}
+    for i = 1, num_leds do
+        local led = {}
+        led.led_name_len, pos = utils.unpack_u16(s, pos)
+        led.led_name, pos = utils.unpack_string(s, pos, led.led_name_len)
+        led.led_value, pos = utils.unpack_u32(s, pos)
+        table.insert(leds,led)
+    end
+    return leds, pos
+end
+
+function utils.unpack_LED_Alt_Names(s, pos, num_led_alt_names)
+    local alt_names = {}
+    for i = 1, num_led_alt_names do
+        local alt_name = {}
+        alt_name.led_alt_name_len, pos = utils.unpack_u16(s, pos)
+        alt_name.led_alt_name, pos = utils.unpack_string(s, pos, alt_name.led_alt_name_len)
+        table.insert(alt_names, alt_name)
+    end
+    return alt_names, pos
+end
+
+function utils.unpack_Controller_Data(data)
+    local controller_data = {}
+    local pos = 1
+    controller_data.data_size, pos = utils.unpack_u32(data, pos)
+    controller_data.type, pos = utils.unpack_i32(data, pos)
+    controller_data.name_len, pos = utils.unpack_u16(data, pos)
+    controller_data.name, pos = utils.unpack_string(data, pos, controller_data.name_len)
+    controller_data.vendor_len, pos = utils.unpack_u16(data, pos)
+    controller_data.vendor, pos = utils.unpack_string(data, pos, controller_data.vendor_len)
+    controller_data.description_len, pos = utils.unpack_u16(data, pos)
+    controller_data.description, pos = utils.unpack_string(data, pos, controller_data.description_len)
+    controller_data.version_len, pos = utils.unpack_u16(data, pos)
+    controller_data.version, pos = utils.unpack_string(data, pos, controller_data.version_len)
+    controller_data.serial_len, pos = utils.unpack_u16(data, pos)
+    controller_data.serial, pos = utils.unpack_string(data, pos, controller_data.serial_len)
+    controller_data.location_len, pos = utils.unpack_u16(data, pos)
+    controller_data.location, pos = utils.unpack_string(data, pos, controller_data.location_len)
+    controller_data.num_modes, pos = utils.unpack_u16(data, pos)
+    controller_data.active_mode, pos = utils.unpack_i32(data, pos)
+    controller_data.modes, pos = utils.unpack_Mode_Data(data, pos, controller_data.num_modes)
+    controller_data.num_zones, pos = utils.unpack_u16(data, pos)
+    controller_data.zones, pos = utils.unpack_Zone_Data(data, pos, controller_data.num_zones)
+    controller_data.num_leds, pos = utils.unpack_u16(data, pos)
+    controller_data.leds, pos = utils.unpack_LED_Data(data, pos, controller_data.num_leds)
+    controller_data.num_colors, pos = utils.unpack_u16(data, pos)
+    controller_data.colors, pos = utils.unpack_RGBColorN(data, pos, controller_data.num_colors)
+    controller_data.num_led_alt_names, pos = utils.unpack_u16(data, pos)
+    controller_data.led_alt_names, pos = utils.unpack_LED_Alt_Names(data, pos, controller_data.num_led_alt_names)
+    controller_data.flags, pos = utils.unpack_u32(data, pos)
+    print(dump(controller_data))
+    return controller_data
+end
